cd1305187b4d8844b668c4450f9eeb49045c4d0e
Victor Hugo Marques Rosa Leite <hvictorleite@gmail.com.br>
2023-03-20 17:12:24 -0300

    first commit

---
+++ layout 2/js/script.js
@@ -0,0 +1,40 @@
+// var search = document.querySelector('.search');
+var buscar = document.querySelector('.buscar');
+// var close = document.querySelector('.close');
+var menuIcon = document.querySelector('.menu_icon');
+var closeMenu = document.querySelector('.close_menu');
+var menu = document.querySelector('.menu');
+
+function fecharMenu(){
+    menuIcon.style.display = 'block';
+    closeMenu.style.display = 'none';
+    menu.style.width = "0"
+}
+
+// function fecharBusca(){
+//     buscar.style.maxHeight = "0";
+//     search.style.display = "block";
+//     close.style.display ='none' ;
+// }
+
+// search.addEventListener('click', ()=>{
+//     buscar.style.maxHeight = "20vh";
+//     search.style.display = 'none';
+//     close.style.display = "block";
+//     fecharMenu();
+// });
+
+// close.addEventListener('click', ()=>{
+//     fecharBusca()
+// });
+
+menuIcon.addEventListener('click', ()=>{
+    menuIcon.style.display = 'none';
+    closeMenu.style.display = 'block';
+    menu.style.width = "60%";
+    // fecharBusca()
+});
+
+closeMenu.addEventListener('click', ()=>{
+    fecharMenu();
+});
